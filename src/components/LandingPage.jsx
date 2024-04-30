d44343ac0ec02a5c82a8caa675ba9dc086a94ee7
AishwrayaShimpi <aishwaryashimpi2001@gmail.com>
2024-05-01 01:27:46 +0530

    Final commit

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,20 +1,31 @@
+import { motion } from "framer-motion";
 import React from "react";
 
 function LandingPage() {
   return (
-    <div className="w-full h-screen bg-zinc-900 pt-1">
+    <div data-scroll data-scroll-section data-scroll-speed="-.3" className="w-full h-screen bg-zinc-900 pt-1">
       <div className="textstructure mt-52 px-20">
         {["We Create", "Eye Opening", "Presentation"].map((item, index) => {
           return (
             <div className="masker">
-              <h1 className="uppercase text-[6.5vw] leading-[5vw] tracking-tighter font-Founders-Grotesk font-medium">
-                {item}
-              </h1>
+              <div className="w-fit flex items-end overflow-hidden">
+                {index === 1 && (
+                  <motion.div
+                    initial={{ width: 0 }}
+                    animate={{ width: "6.5vw" }}
+                    transition={{ ease: [0.76, 0, 0.24, 1], duration : 1 }}
+                    className="mr-[1vw] w-[6.5vw] h-[5vw]  relative top-[0.4vw] bg-red-500"
+                  ></motion.div>
+                )}
+                <h1 className="uppercase text-[6.5vw] leading-[5.5vw] tracking-tighter font-Founders-Grotesk font-bold">
+                  {item}
+                </h1>
+              </div>
             </div>
           );
         })}
       </div>
-      <div className="border-t-[1px] border-zinc-800 mt-32 flex justify-between items-center py-5 px-20">
+      <div className="border-t-[1px] border-zinc-800 mt-20 flex justify-between items-center py-5 px-20">
         {[
           "For public and private companies",
           "From the first pitch to IPO",
@@ -24,7 +35,9 @@ function LandingPage() {
           </p>
         ))}
         <div className="start">
-          <div className="px-5 py-2 border-[2px] border-zinc-500 font-light text-sm capitalize rounded-full">Start the project</div>
+          <div className="px-5 py-2 border-[2px] border-zinc-500 font-light text-sm capitalize rounded-full">
+            Start the project
+          </div>
         </div>
       </div>
     </div>
